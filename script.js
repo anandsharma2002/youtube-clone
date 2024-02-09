1801dc45d17b0594379827af6e28fdcd767d98b1
Anand Sharma <anandsharma.ts3000@gmail.com>
2024-02-09 13:28:07 +0530

    updated

---
+++ script.js
@@ -0,0 +1,53 @@
+const videoCardContainer = document.querySelector('.video-container')
+
+let api_key = "AIzaSyDiakEJ21lIGpabQnCwJc0ZYDSwECYtzqM";
+let video_http = "https://www.googleapis.com/youtube/v3/videos?";
+let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+
+fetch(video_http + new URLSearchParams({
+    key: api_key,
+    part: 'snippet',
+    chart: 'mostPopular',
+    maxResults: 50,
+    regionCode: 'IN',
+}))
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data);
+        data.items.forEach(item => {
+            getChannelIcon(item);
+        });
+    })
+    .catch(err => console.log(err));
+
+
+const getChannelIcon = (video_data) => {
+    fetch(channel_http + new URLSearchParams({
+        key: api_key,
+        part: 'snippet',
+        id: video_data.snippet.channelId
+    }))
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+            video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+            console.log(video_data);
+            makeVideoCard(video_data);
+        })
+}
+
+const makeVideoCard = (data) => {
+    videoCardContainer.innerHTML += `
+    <div class="video" onclick = "location.href = 'https://youtube.com/watch?v=${data.id}'">
+        <img class="thumbnail" src="${data.snippet.thumbnails.high.url}" alt="thumbnail">
+        <div class="content">
+            <img class="channel-icon" src="${data.channelThumbnail}" alt="channel-icon">
+            <div class="info">
+                <h4 class="title">${data.snippet.title}</h4>
+                <p class="channel-name">${data.snippet.channelTitle}</p>
+            </div>
+        </div>
+    </div>
+    `
+    
+}
